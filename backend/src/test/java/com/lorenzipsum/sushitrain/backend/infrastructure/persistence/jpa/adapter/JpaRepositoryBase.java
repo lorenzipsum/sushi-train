e1c1e433e31bb30ddf49cea0faec907eb8863202
lorenzo <lorenz.techuser@gmail.com>
2026-01-09 00:14:08 +0800

    refactor: switched to spring boot version 4

--- backend/src/test/java/com/lorenzipsum/sushitrain/backend/infrastructure/persistence/jpa/adapter/JpaRepositoryBase.java
+++ backend/src/test/java/com/lorenzipsum/sushitrain/backend/infrastructure/persistence/jpa/adapter/JpaRepositoryBase.java
@@ -1,9 +1,8 @@
 package com.lorenzipsum.sushitrain.backend.infrastructure.persistence.jpa.adapter;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
@@ -11,22 +10,8 @@ import static com.lorenzipsum.sushitrain.backend.IntegrationTestDatabase.create;
 
 public class JpaRepositoryBase {
     @Container
-    static final PostgreSQLContainer db = create();
+    @ServiceConnection
+    private static final PostgreSQLContainer db = create();
     @Autowired
     TestEntityManager em;
-
-    @DynamicPropertySource
-    static void registerProps(DynamicPropertyRegistry r) {
-        registerDynamicProperties(r, db);
-    }
-
-    public static void registerDynamicProperties(DynamicPropertyRegistry r, PostgreSQLContainer DB) {
-        r.add("spring.datasource.url", DB::getJdbcUrl);
-        r.add("spring.datasource.username", DB::getUsername);
-        r.add("spring.datasource.password", DB::getPassword);
-        // prefer Flyway-managed schema
-        r.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
-        r.add("spring.flyway.enabled", () -> "true");
-        r.add("spring.flyway.locations", () -> "classpath:db/migration");
-    }
 }
