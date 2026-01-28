c748fb29c32ba508611bc38248e3f78c213281c3
lorenzo <lorenz.techuser@gmail.com>
2026-01-28 11:40:00 +0700

    reactivated default SpringBoot test using test containers and flyway

--- backend/src/test/java/com/lorenzipsum/sushitrain/backend/SushiTrainBackendApplicationTests.java
+++ backend/src/test/java/com/lorenzipsum/sushitrain/backend/SushiTrainBackendApplicationTests.java
@@ -1,12 +1,24 @@
 package com.lorenzipsum.sushitrain.backend;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 class SushiTrainBackendApplicationTests {
 
-    //  currently now dev database to connect to, use infrastructure tests instead
-    //	@Test
+    @Container
+    @ServiceConnection
+    @SuppressWarnings("unused")
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:18-alpine");
+
+    @Test
     void contextLoads() {
     }
 
